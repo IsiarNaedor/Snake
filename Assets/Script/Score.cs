dd60becb21a80acbe276067293cc894136e33843
Will Hui <willhui0414@gmail.com>
2025-02-18 10:54:56 -0500

    Updated score timer

--- Assets/Script/Score.cs
+++ Assets/Script/Score.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public Text scoreText;
+    private float elapsedTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,25 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //scoreText.text += 1;
+        elapsedTime += Time.deltaTime;
+        scoreText.text = Mathf.FloorToInt(elapsedTime).ToString();
     }
 }
+//using UnityEngine;
+//using UnityEngine.UI;
+//using TMPro;
+
+//public class Score : MonoBehaviour
+//{
+//    public Text scoreText;
+//    private float elapsedTime;
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        Debug.Log(elapsedTime);
+//        elapsedTime += Time.deltaTime;
+//        scoreText.text = string.Format("0", elapsedTime);
+//        scoreText.text += 1;
+//    }
+//}
